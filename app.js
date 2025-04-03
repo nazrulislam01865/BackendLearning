0de9e7575e5d5b8d170f665fe1a42951c25d497d
Nazrul Islam <nazrulislam@Nazruls-MacBook.local>
2025-04-03 13:35:25 +0600

    firstsecoond

--- app.js
+++ app.js
@@ -1,14 +1,47 @@
-const cookieParser = require('cookie-parser');
 const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+
+
 const app = express();
+
 app.use(cookieParser());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/', function(req, res) {
-    res.cookie('name', 'express');
+    // //hash password
+    // bcrypt.genSalt(10, function(err, salt) {
+    //     bcrypt.hash('myPassword', salt, function(err, hash) {
+    //        // console.log(hash);
+            
+    //     });
+    // });
+    //check password
+    // bcrypt.compare('myPassword', '$2b$10$L8E2WSEDdV6QDC6OOxe3LenAkv78sl2C9WTHvk8.iP6IuHIwUwVW.', function(err, result) {
+    //     console.log(result); // true
+    // }
+    // );
+
+    //jwt token
+   let token= jwt.sign({email: "nazrul@gmail.com"},"secretkey");
+   res.cookie('token',token)
+   console.log(token);
     res.send('Hello World!');
+ 
+})
+
+//verifytoken
+app.get('/read', function(req, res) {
+    let data = jwt.verify(req.cookies.token, "secretkey");
+    console.log(data);
+
 });
+
+// app.get('/read', function(req, res) {
+//     console.log(req.cookie());
+//     res.send('Hello World!');
+// });
 
 app.listen(3000);
